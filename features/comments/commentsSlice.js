605804eed75de16ebedef2cd00f1b61f7ada1a87
Kekai Newman <kekainewman@gmail.com>
2023-10-28 17:49:27 -1000

    Workshop Week 2

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -15,10 +15,32 @@ export const fetchComments = createAsyncThunk(
     }
 );
 
+export const postComment = createAsyncThunk(
+    'comments/postComment',
+    async (payload, {dispatch, getState}) => {
+        try {
+            return new Promise(() => {
+                setTimeout(() => {
+                    payload.date = new Date().toISOString();
+                    const { comments } = getState();
+                    payload.id = comments.commentsArray.length;
+                    dispatch(addComment(payload));
+                }, 2000);
+            });
+        } catch (error) {
+            return Promise.reject('Failed to post comment: ' + error.message);
+        }
+    }
+);
+
 const commentsSlice = createSlice({
     name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: {
+        addComment: (state, action) => {
+            state.commentsArray.push(action.payload)
+        }
+    },
     extraReducers: (builder) => {
         builder
             .addCase(fetchComments.pending, (state) => {
@@ -38,4 +60,5 @@ const commentsSlice = createSlice({
     }
 });
 
+export const { addComment } = commentsSlice.actions;
 export const commentsReducer = commentsSlice.reducer;
